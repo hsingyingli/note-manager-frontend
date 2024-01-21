d42cdc77f89e8a710b70d78bc51a333a0699521d
Aaron Li <58147531+hsingyingli@users.noreply.github.com>
2024-01-21 19:10:05 +0800

    feat: Add Navbar (#4)
    * chore: add logo.png
    
    * fix(ThemeSwitcher): add use client
    
    * feat: add Navbar
    
    * feat: Layout add Navbar and ThemeSwitcher
    
    * fix(AuthLayout): calc height

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ interface Props {
 // Note: add simple navbar and footer?
 const AuthLayout = ({ children }: Props) => {
   return (
-    <div className='min-h-screen flex items-center justify-center px-1.5'>
+    <div className='min-h-[calc(100vh_-_60px)] flex items-center justify-center px-1.5'>
       {children}
     </div>
   )
