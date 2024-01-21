f6eb98de67022664d1fda8f6779b016397dc14dd
hsingyingli <xingyingli855@gmail.com>
2024-01-21 16:30:01 +0800

    feat(lib/validation): add username validation rule

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,7 +1,11 @@
 import * as z from "zod"
 
 const passwordRegexPattern = /^[0-9a-zA-Z]{8,12}$/;
+const usernameRegexPattern = /^[0-9a-zA-Z]{6,18}/;
 
+export const usernameValidation = z.string().regex(usernameRegexPattern, {
+  message: 'Username must be 6~18 characters. Only accept 1~9, a-z and A-Z are accepted'
+})
 
 export const passwordValidation = z.string().regex(passwordRegexPattern, {
     message: 'Password must be 8~12 characters. Only accept 1~9, a-z and A-Z are accepted'
