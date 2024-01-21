93eefdb8859f997a4571ee38b3a1d9048f8887f0
Aaron Li <58147531+hsingyingli@users.noreply.github.com>
2024-01-21 16:08:48 +0800

    feat: Add Login Page (#2)
    * feat: ad shadcn Card
    
    * chore: install shadcn Form, Input, Label
    
    * feat(lib/validation): add password and email validation rule
    
    * feat: add auth related page layout
    
    * feat: add login form
    
    * feat: add login page

---
+++ src/app/(auth)/login/_components/Form.tsx
@@ -0,0 +1,77 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { emailValidation, passwordValidation } from "@/lib/validation"
+
+
+
+
+const formSchema = z.object({
+  email: emailValidation,
+  password: passwordValidation
+})
+
+const LoginForm = () => {
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: '',
+    },
+  })
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" placeholder="example@example.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
+
+export default LoginForm
