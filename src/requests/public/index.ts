e04bfba34ee479209b80ac9d92ae3858f6752370
hsingyingli <xingyingli855@gmail.com>
2024-04-13 14:05:03 +0800

    feat: split private and public api

---
+++ src/requests/public/index.ts
@@ -0,0 +1,32 @@
+import axios from "@/requests/axios";
+
+export type LoginUserParam = {
+  email: string,
+  password: string,
+}
+
+export const loginUser = ({ email, password }: LoginUserParam) => axios({
+  url: '/api/v1/user/login',
+  method: 'POST', 
+  withCredentials: true,
+  data: {email, password}
+})
+
+export type RegisterUserParam = {
+  username: string, 
+  email: string,
+  password: string,
+}
+
+export const registerUser = ({ username, email, password }: RegisterUserParam) => axios({
+  url: '/api/v1/user',
+  method: 'POST',
+  data: {username, email, password}
+})
+
+export const logoutUser = () => axios({
+  url: '/api/v1/user/logout',
+  method: 'POST'
+})
+
+
