3ad2e14c7900c83e7d4e35e167b099c52b933555
hsingyingli <xingyingli855@gmail.com>
2024-04-13 14:05:27 +0800

    feat: integrate react query provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Toaster } from "@/components/ui/toaster"
 import ThemeProvider from '@/stores/providers/ThemeProvider'
 import NavBar from '@/components/NavBar'
+import UserProvider from '@/stores/providers/UserProvider'
+import ReactQueryProvider from '@/stores/providers/ReactQueryProvider'
 
 const inter = Inter({ subsets: ['latin'] })
+
+
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,10 +24,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider>
-          <NavBar />
-          {children}
-        </ThemeProvider>
+        <ReactQueryProvider>
+          <ThemeProvider>
+            <UserProvider>
+              <NavBar />
+              {children}
+              <Toaster />
+            </UserProvider>
+          </ThemeProvider>
+        </ReactQueryProvider>
       </body>
     </html>
   )
